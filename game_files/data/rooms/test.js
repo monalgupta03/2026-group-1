a621c3ac3a859ba6ec5fa6297fa05e8984d4facf
Georgia Sweeny <124116920+GeorgiaSweeny@users.noreply.github.com>
2026-02-16 18:43:14 +0000

    Create test.js

---
+++ game_files/data/rooms/test.js
@@ -0,0 +1,40 @@
+{
+  "height": 12,
+  "width": 20,
+  "tileheight": 32,
+  "tilewidth": 32,
+  "layers": [
+    {
+      "type": "objectgroup",
+      "name": "platforms",
+      "objects": [
+        { "x": 50, "y": 296, "width": 160, "height": 16 },
+        { "x": 225, "y": 248, "width": 112, "height": 16 },
+        { "x": 352, "y": 216, "width": 96, "height": 16 },
+        { "x": 480, "y": 248, "width": 96, "height": 16 }
+      ]
+    },
+    {
+      "type": "objectgroup",
+      "name": "spawns",
+      "objects": [
+        { "name": "player", "type": "player", "x": 74, "y": 254, "width": 32, "height": 32 }
+      ]
+    },
+    {
+      "type": "objectgroup",
+      "name": "entities",
+      "objects": []
+    },
+    {
+      "type": "objectgroup",
+      "name": "exits",
+      "objects": []
+    }
+  ],
+  "properties": [
+    { "name": "backgroundColor", "type": "string", "value": "#0f1b2e" },
+    { "name": "backgroundImage", "type": "string", "value": "forrest.png" },
+    { "name": "platformColor", "type": "string", "value": "#0a1016" }
+  ]
+}
